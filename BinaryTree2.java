9635381eb4a0526bc823a21eba53d02cb79a1397
Om Prakash Mallik <omprakashmallik91@gmail.com>
2023-11-09 08:17:52 +0530

    tree day 2

---
+++ BinaryTree2.java
@@ -0,0 +1,25 @@
+public class BinaryTree2 {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node (int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        public static buildTree(int nodes){
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree tree = new BinaryTree();
+    }
+}
