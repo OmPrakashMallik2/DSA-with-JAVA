0a88026481bb636a46f988f3b6a2515f932da222
Om Prakash Mallik <omprakashmallik91@gmail.com>
2023-11-09 08:37:15 +0530

    impemented binary tree

--- BinaryTree2.java
+++ BinaryTree2.java
@@ -1,3 +1,4 @@
+
 public class BinaryTree2 {
 
     static class Node {
@@ -5,7 +6,7 @@ public class BinaryTree2 {
         Node left;
         Node right;
 
-        Node (int data){
+        Node(int data) {
             this.data = data;
             this.left = null;
             this.right = null;
@@ -13,13 +14,26 @@ public class BinaryTree2 {
     }
 
     static class BinaryTree {
-        public static buildTree(int nodes){
+        static int idx = -1;
 
+        public Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
         }
     }
 
     public static void main(String[] args) {
-        int nodes[] = { 1, 2, 4, -1, -1, 3, -1, 6, -1, -1 };
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.println(root.data);
     }
 }
