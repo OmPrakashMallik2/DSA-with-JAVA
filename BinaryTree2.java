7aa56e147607d6c654fa060383073143a6c4e347
Om Prakash Mallik <omprakashmallik91@gmail.com>
2023-11-09 17:46:22 +0530

    implemented binary tree successfully

--- BinaryTree2.java
+++ BinaryTree2.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 public class BinaryTree2 {
 
@@ -30,10 +31,68 @@ public class BinaryTree2 {
         }
     }
 
+    public static void preorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void postorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    public static void levelOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while (!q.isEmpty()) {
+            Node curNode = q.remove();
+
+            if (curNode == null) {
+                System.out.println();
+                if (q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                System.out.print(curNode.data + " ");
+                if (curNode.left != null) {
+                    q.add(curNode.left);
+                }
+                if (curNode.right != null) {
+                    q.add(curNode.right);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        System.out.println(root.data);
+        // System.out.println(root.data);
+        levelOrder(root);
     }
 }
