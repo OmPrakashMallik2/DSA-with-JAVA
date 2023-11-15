192419e24b7cabc8f7ddd6b726455885986d1778
Om Prakash Mallik <omprakashmallik91@gmail.com>
2023-11-15 18:58:05 +0530

    new problem

---
+++ BT1.java
@@ -0,0 +1,31 @@
+
+class Node {
+
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    Node(int data, Node left) {
+        this.data = data;
+        this.left = left;
+        this.right = null;
+    }
+
+    Node(int data, Node left, Node right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class BT1 {
+    public static void main(String[] args) {
+
+    }
+}
